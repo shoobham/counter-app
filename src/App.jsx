9af30b5b02d7a4de0a2a4f3d82ee61491ff1510f
Shubham Jain <shubham@Shubhams-MacBook.local>
2024-02-04 12:55:30 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+    <div>
+        <CustomButton count={count} setCount={setCount}></CustomButton>
+        <CustomButton count={count + 1} setCount={setCount}></CustomButton>
+        <CustomButton count={count / 2} setCount={setCount}></CustomButton>
+        <CustomButton count={count * 10} setCount={setCount}></CustomButton>
+    </div>
+  )
+}
+
+//component
+function CustomButton(props) {
+    function onClickHandler() {
+        props.setCount(props.count + 1);
+    }
+    return <button onClick={onClickHandler}>
+        Counter {props.count}
+    </button>
+}
+
+export default App
